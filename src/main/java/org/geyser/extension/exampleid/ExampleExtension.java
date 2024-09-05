b67e45801930009096624949d1bb8e951224dddb
Outfit8TSB <sunnybunnies09@naver.com>
2024-09-05 19:54:25 +0900

    Update ExampleExtension.java

--- src/main/java/org/geyser/extension/exampleid/ExampleExtension.java
+++ src/main/java/org/geyser/extension/exampleid/ExampleExtension.java
@@ -11,23 +11,13 @@ import java.nio.file.Path;
 // see https://github.com/GeyserMC/Geyser/blob/master/api/src/main/java/org/geysermc/geyser/api/extension/Extension.java for more info on available methods
 public class ExampleExtension implements Extension {
 
-    // Registering custom items/blocks, or adding resource packs (and basically all other events that are fired before Geyser initializes fully)
-    // are done in their respective events. See below for an example:
-    @Subscribe
-    public void onGeyserLoadResourcePacksEvent(GeyserLoadResourcePacksEvent event) {
-        logger().info("Loading: " + event.resourcePacks().size() + " resource packs.");
-        // you could add a resource pack with event.resourcePacks().add(path-to-pack)
-    }
-
     // You can use the GeyserPostInitializeEvent to run anything after Geyser fully initialized and is ready to accept bedrock player connections.
     @Subscribe
     public void onPostInitialize(GeyserPostInitializeEvent event) {
         // example: show that your extension is loading.
-        this.logger().info("Loading example extension...");
-        
-        //example: accessing extension data folder
-        Path exampleDataFolder = this.dataFolder();
-        this.logger().info(exampleDataFolder.toString());
+        this.logger().info("AnarchyConnect(TM)");
+        this.logger().info("The Ultimate Game System");
+        this.logger().info("You Cannot BEAT US");
     }
 
 }
